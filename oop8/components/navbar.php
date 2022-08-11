0c6dc29f78e76cb5360064360069f27620fbe97e
clem0802 <hhcc0802@gmail.com>
2022-08-11 13:39:15 +0200

    --

--- oop8/components/navbar.php
+++ oop8/components/navbar.php
@@ -2,23 +2,23 @@
 <?php 
     $links = [
         [
-            'url' => '../oop8/index.php',
+            'url' => '/y.doranco-6-OOP/oop8/index.php',
             'name' => '(0) Accueil (index.php)',
         ],
         [
-            'url' => '/oop8/revision/1type-var.php',
+            'url' => '/y.doranco-6-OOP/oop8/revision/1type-var.php',
             'name' => '(1) Type & Variable',
         ],
         [
-            'url' => '../oop8/revision/2condition.php',
+            'url' => '/y.doranco-6-OOP/oop8/revision/2condition.php',
             'name' => '(2) Condition',
         ],
         [
-            'url' => '../oop8/revision/3loop-array.php',
+            'url' => '/y.doranco-6-OOP/oop8/revision/3loop-array.php',
             'name' => '(3) Loop & Array',
         ],
         [
-            'url' => '../oop8/revision/4function.php',
+            'url' => '/y.doranco-6-OOP/oop8/revision/4function.php',
             'name' => '(4) Fonction',
         ],
     ]
@@ -26,7 +26,7 @@
 // SVG: Scalable Vector Graphic
 ?>
 
-<link rel="stylesheet" href="../oop8/styles/navbar.css">
+<link rel="stylesheet" href="/y.doranco-6-OOP/oop8/styles/navbar.css">
 
 <nav>
     <svg id="menuButton" width="32" height="32" viewBox="0 0 1080 1080" fill="#245195">
@@ -48,4 +48,4 @@
     </ul>
 </nav>
 
-<script src="../oop8/script/navbar.js"></script>
+<script src="/y.doranco-6-OOP/oop8/script/navbar.js"></script>
